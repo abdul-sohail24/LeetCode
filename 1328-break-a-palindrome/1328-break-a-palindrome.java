b7511a0ede1d2c542b4ad115c686e3e3608246f7
Mohammed Abdul Sohail <abdulsohail22246@gmail.com>
2022-10-10 10:24:53 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (89.54%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -2,13 +2,14 @@ class Solution
 {
     public String breakPalindrome(String palindrome) 
     {
-        if(palindrome.length() == 1)
+        int n = palindrome.length();
+        if(n == 1)
             return "";
         
         char[] palin_array = palindrome.toCharArray();
         int flip = 1;
         
-        for(int i=0; i<palin_array.length/2; i++)
+        for(int i=0; i<n/2; i++)
         {
             if(palin_array[i] != 'a')
             {
@@ -19,11 +20,11 @@ class Solution
         }
         
         if(flip == 1)
-            palin_array[palindrome.length() - 1] = 'b';
+            palin_array[n - 1] = 'b';
         
         StringBuilder s = new StringBuilder();
     
-        for(int i=0; i<palindrome.length(); i++)
+        for(int i=0; i<n; i++)
             s.append(palin_array[i]);
         
         return s.toString();
