051f73af684f16f6cd9e3d2acfc5d2fea49d5de9
Mohammed Abdul Sohail <abdulsohail22246@gmail.com>
2022-11-24 14:30:31 +0530

    Time: 2 ms (23.17%), Space: 61.1 MB (10.28%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -2,20 +2,10 @@ class Solution
 {
     public int singleNonDuplicate(int[] nums) 
     {
-        //O(N)
-        if(nums.length == 1)
-            return nums[0];
-        for(int i=1; i<nums.length-1; i++)
-        {
-            int count = 1;
-            if(nums[i] == nums[i+1] || nums[i] == nums[i-1])
-                count++;
-            if(count == 1)
-                return nums[i];
-        }
-        if(nums[nums.length-1] != nums[nums.length-2])
-            return nums[nums.length-1];
-        else
-            return nums[0];
+        //O(N) XOR
+        int ans = 0;
+        for(int i=0; i<nums.length; i++)
+            ans ^= nums[i];
+        return ans;
     }
 }
