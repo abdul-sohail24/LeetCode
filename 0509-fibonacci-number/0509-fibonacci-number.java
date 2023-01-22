f39c6e989715b3f87a3b65cadc3b1820367b100c
Mohammed Abdul Sohail <abdulsohail22246@gmail.com>
2023-01-22 13:04:06 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (6.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,21 +1,17 @@
 class Solution 
 {
-    public int fib(int n) 
+    //Tabulation (Bottom-Up)
+    public int fib(int n)
     {
-        int[] dp = new int[n+1];
-        for(int i=0; i<n+1; i++)
-            dp[i] = -1;
-        int res = result(n, dp);
-        return res;
-    }
-    
-    public int result(int n, int[] dp)
-    { 
         if(n <= 1)
             return n;
-        if(dp[n] != -1)
-            return dp[n];
-        dp[n] = result(n-1, dp) + result(n-2, dp);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<n+1; i++)
+            dp[i] = dp[i-1] + dp[i-2];
         return dp[n];
     }
 }
